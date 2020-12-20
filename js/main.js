816dfbff0b1aed84189c24abd7022bd206d1ccd0
Ahmar Ali Khan <mail2ahmaralikhan52@gmail.com>
2020-12-20 23:28:48 +0530

    Added

--- js/main.js
+++ js/main.js
@@ -2,4 +2,7 @@
 function selectItem(e) {
 	// Remove all show and border classes
 	removeBorder();
-	removeShow();
+    removeShow();
+    // Add border to current tab item
+	this.classList.add('tab-border');
+	// Grab content item from DOM
